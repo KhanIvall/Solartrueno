670bc8b7191cbc6e21d6e3505de6c25ec4da7d29
Ninna <gia.guerrero@duocuc.cl>
2025-12-02 16:59:41 -0300

    Se modifica estructura sidemenu y se agrega lista_servicios

--- src/adminlte/pages/components/sidemenu.js
+++ src/adminlte/pages/components/sidemenu.js
@@ -3,14 +3,13 @@ import React from "react";
 function SideMenu() {
     return (
         <aside className="main-sidebar sidebar-dark-primary elevation-4">
-            {/* Brand Logo */}
+
             <a href="index3.html" className="brand-link">
-                <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" className="brand-image img-circle elevation-3" style={{ opacity: '.8' }} />
+                <img src='assets/images/imagotipo.png' alt='logo' style={{ width: '60px', margin: '3px' }} />
                 <span className="brand-text font-weight-light">SolarTrueno</span>
             </a>
-            {/* Sidebar */}
+
             <div className="sidebar">
-                {/* Sidebar user panel (optional) */}
                 <div className="user-panel mt-3 pb-3 mb-3 d-flex">
                     <div className="image">
                         <img src="dist/img/user2-160x160.jpg" className="img-circle elevation-2" alt="User Image" />
@@ -20,39 +19,30 @@ function SideMenu() {
                     </div>
                 </div>
                 
-                {/* Sidebar Menu */}
                 <nav className="mt-2">
                     <ul className="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                         
+                        <li className="nav-header">MENÚ PRINCIPAL</li>
+                        
                         <li className="nav-item">
-                            <a href="#" className="nav-link">
-                                <i className="nav-icon fas fa-book" />
-                                <p>
-                                    Pages
-                                    <i className="fas fa-angle-left right" />
-                                </p>
+                            <a href="pages/examples/invoice.html" className="nav-link">
+                                <i className="nav-icon fas fa-file-invoice" />
+                                <p>Servicios</p>
                             </a>
-                            <ul className="nav nav-treeview">
-                                <li className="nav-item">
-                                    <a href="pages/examples/invoice.html" className="nav-link">
-                                        <i className="far fa-circle nav-icon" />
-                                        <p>Servicios</p>
-                                    </a>
-                                </li>
-                                <li className="nav-item">
-                                    <a href="/plans" className="nav-link">
-                                        <i className="far fa-circle nav-icon" />
-                                        <p>Planes</p>
-                                    </a>
-                                </li>
-                            </ul>
+                        </li>
+
+                        <li className="nav-item">
+                            <a href="/plans" className="nav-link">
+                                <i className="nav-icon fas fa-list-alt" />
+                                <p>Planes</p>
+                            </a>
                         </li>
                         
                     </ul>
                 </nav>
-                {/* /.sidebar-menu */}
+
             </div>
-            {/* /.sidebar */}
+
         </aside>
 
     );
